2903369aa8b23f542ccc6b1035fb2d4d28695d65
Hendi Santika <hendisantika@gmail.com>
2018-04-24 05:24:23 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/kotlin/thymeleaf/SpringBootKotlinThymeleafApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.kotlin.thymeleaf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinThymeleafApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinThymeleafApplication>(*args)
+}
