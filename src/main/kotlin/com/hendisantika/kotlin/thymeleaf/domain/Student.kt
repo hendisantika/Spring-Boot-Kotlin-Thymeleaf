5523b5d90c03a25ba562960226c039f635e25cf9
Hendi Santika <hendisantika@gmail.com>
2018-04-25 23:13:47 +0700

    #3 - Add Student Data Class

---
+++ src/main/kotlin/com/hendisantika/kotlin/thymeleaf/domain/Student.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.kotlin.thymeleaf.domain
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kotlin-thymeleaf-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/04/18
+ * Time: 23.13
+ * To change this template use File | Settings | File Templates.
+ */
+class Student(val studentId: String, val lastName: String, val firstName: String)
