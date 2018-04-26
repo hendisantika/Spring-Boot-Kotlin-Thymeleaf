ed1e37df1d5066ef51e01be0d51205807fc449c9
Hendi Santika <hendisantika@gmail.com>
2018-04-26 07:48:13 +0700

    #4 - Add Student Form Data Class

---
+++ src/main/kotlin/com/hendisantika/kotlin/thymeleaf/form/CreateStudentForm.kt
@@ -0,0 +1,17 @@
+package com.hendisantika.kotlin.thymeleaf.form
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kotlin-thymeleaf-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/04/18
+ * Time: 07.47
+ * To change this template use File | Settings | File Templates.
+ */
+class CreateStudentForm {
+    var studentId: String? = null
+    var lastName: String? = null
+    var firstName: String? = null
+}
